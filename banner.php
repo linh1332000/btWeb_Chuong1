12ee5b610a1a14214e330222afe7a4ca26bb4ebc
Linh <Linh@blinh8358@gmail.com>
2021-09-16 12:53:19 +0700

    first commit

---
+++ banner.php
@@ -0,0 +1,12 @@
+<div id="banner" class="container-fuild " style="margin-top: 50px; height:400px;">
+    <div class="container">
+
+        <form action="food-search.html" method="POST">
+            <input
+                style="margin-top:15%;margin-left:25%;width:50%;height:50px;border: 2px; -moz-border-radius: 10px; -webkit-border-radius: 10px; -ms-border-radius: 10px;-o-border-radius: 10px;border-radius: 10px;"
+                type="search" name="search" placeholder="Search for Food.." required>
+            <input style="height:50px;" type="submit" name="submit" value="Search" class="btn btn-primary">
+        </form>
+
+    </div>
+</div>
